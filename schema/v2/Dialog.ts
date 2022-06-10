ea32d2fc1de96f338c9008cb77a42fae0b9ec018
Andrey Kniazev <worldpwn@hotmail.com>
2022-06-10 12:33:33 +0200

    Add basic folder structure and basic components

---
+++ schema/v2/Dialog.ts
@@ -0,0 +1,6 @@
+import { Component } from '../Component';
+
+export interface Dialog extends Component {
+  type: '@lorehub/dialog',
+  apiVersion: '2.0.0',
+}
---
+++ schema/v3/Dialog.ts
@@ -0,0 +1,6 @@
+import { Component } from '../Component';
+
+export interface Dialog extends Component {
+  type: '@lorehub/dialog',
+  apiVersion: '3.0.0',
+}
